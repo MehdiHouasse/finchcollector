0f5f0ac7024aea03fb229242dc273f6e067fa497
MehdiHouasse <mehdi.houasse1@gmail.com>
2023-09-22 14:50:22 -0700

    First commit

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def all_finch(request):
+    return render(request, 'all.html')
